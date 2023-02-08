a14fa622c441501b282485fb2004adfebd0d6213
Milo Critchett <2651413c@student.gla.ac.uk>
2023-02-08 17:38:09 +0000

    Fixed bug on chapter 5 tests

--- rango/admin.py
+++ rango/admin.py
@@ -7,8 +7,10 @@ class PageAdmin(admin.ModelAdmin):
 	('category', {'fields':['category']}),
 	('URL', {'fields':['url']})
 	]
-	
 	list_display = ('title', 'category', 'url')
+	
+class CategoryAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug':('name',)}
 
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
