cb2a4382f90daadac8c416d36538b28fe0602af8
Milo Critchett <2651413c@student.gla.ac.uk>
2023-02-09 08:40:33 +0000

    Fixed bug on chapter 6 tests

--- rango/views.py
+++ rango/views.py
@@ -4,9 +4,11 @@ from rango.models import Category, Page
 
 def index(request):
 	category_list = Category.objects.order_by('-likes')[:5]
+	pages_list = Page.objects.order_by('-views')[:5]
 	context_dict = {}
 	context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
 	context_dict['categories'] = category_list
+	context_dict['pages'] = pages_list
 	return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
